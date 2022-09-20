4151145be0f2dd0f92aed9173627682f2ae9fae1
Ivan Gonzalez Valdes <kaosolution8@gmail.com>
2022-09-20 09:42:18 -0400

    deploy in heroku v7

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -57,7 +57,7 @@ module.exports = configure(function (ctx) {
           : '/api/'
       },
 
-      vueRouterMode: 'history', // available values: 'hash', 'history'
+      vueRouterMode: 'hash', // available values: 'hash', 'history'
 
       // transpile: false,
       // publicPath: '/',
